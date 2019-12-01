2a2c55a4add1ff077a2db3a427e6e6e2a63ebc91
sankkris1 <45379663+sankkris1@users.noreply.github.com>
2019-11-30 22:30:34 -0500

    updated

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class HelloWorld {
+public class Helloworld {
   public static void main(String[] args) {
     System.out.println("Hello, World Sankari upd in new branch");
   }
